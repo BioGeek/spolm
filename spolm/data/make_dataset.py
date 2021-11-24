d8ef61d78131150b989fb7198c5fdf09be2fea53
Jeroen Van Goey <jeroen.vangoey@gmail.com>
2021-11-24 16:27:34 +0100

    Add make_dataset function to download slices of raw data

--- spolm/data/make_dataset.py
+++ spolm/data/make_dataset.py
@@ -3,32 +3,49 @@ import logging
 from pathlib import Path
 
 import click
+import pandas as pd
 from dotenv import find_dotenv
 from dotenv import load_dotenv
 
+RAW_DATA_URL = (
+    "https://raw.githubusercontent.com/mcnakhaee/palmerpenguins/"
+    "master/palmerpenguins/data/penguins-raw.csv"
+)
+
 
 @click.command()
-@click.argument("input_filepath", type=click.Path(exists=True))
-@click.argument("output_filepath", type=click.Path())
-def main(input_filepath: Path, output_filepath: Path) -> None:
-    """Make a dataset from raw data.
-
-    Runs data processing scripts to turn raw data from (../raw) into
-    cleaned data ready to be analyzed (saved in ../processed).
-    """
+@click.option(
+    "--nr_lines", default=10, help="Number of lines to add to the raw dataset"
+)
+def get_raw_data_slice(nr_lines: int) -> None:
+    """Download `nr_of_lines` (more) of raw data."""
     logger = logging.getLogger(__name__)
-    logger.info("making final data set from raw data")
+    project_dir = Path(__file__).resolve().parents[2]
+    filename = RAW_DATA_URL.rsplit("/", 1)[-1]
+    output_filepath = project_dir / "data" / "raw" / filename
+    df = pd.read_csv(RAW_DATA_URL)
+    if not output_filepath.exists():
+        logger.info(f"Making first raw data set with {nr_lines} lines")
+        df = df.head(nr_lines)
+        df.to_csv(output_filepath, index=False)
+    else:
+        with open(output_filepath) as f:
+            current_nr_lines = len(f.readlines()) - 1  # remove 1 for the header
+        new_nr_lines = current_nr_lines + nr_lines
+        logger.info(
+            f"Updating raw data set. Already found {current_nr_lines}, "
+            f"adding {nr_lines} lines"
+        )
+        df = df.iloc[:new_nr_lines, :]
+        df.to_csv(output_filepath, index=False)
 
 
 if __name__ == "__main__":
     log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
     load_dotenv(find_dotenv())
 
-    main()
+    get_raw_data_slice()
